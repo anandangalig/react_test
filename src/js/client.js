06ed3114b108ec785ebb546b0611f20232200573
Anand Angalig <anandangalig@gmail.com>
2017-03-01 09:21:31 -0500

    added webpack-dev-server: watches JS changes and auto-refreshes with changes on localhost:8080

--- src/js/client.js
+++ src/js/client.js
@@ -5,10 +5,10 @@ import ReactDOM from 'react-dom';
 var Button = React.createClass({
   render: function() {
     return (
-      <button onClick={this.props.localHandleClick}>+1</button>
+      <button class="btn btn-success" onClick={this.props.localHandleClick}>+1</button>
     )
   }
-});
+})
 
 //Defining another component:
 var Result = React.createClass({
@@ -29,12 +29,13 @@ var Main = React.createClass({
   },
   render: function() {
     return (
-      <div>
+      <div class="container">
+        <h1>React JS is Working!!!</h1>
         <Button localHandleClick={this.handleClick} />
         <Result localCounter={this.state.counter} />
       </div>
     )
   }
-})
+});
 
-ReactDOM.render(<Main />, document.getElementById("container")); // components rendered to html. 1st argument is the Main component that has all the rest in it, 2nd argument is the mountNode.
+ReactDOM.render(<Main />, document.getElementById("root")); // components rendered to html. 1st argument is the Main component that has all the rest in it, 2nd argument is the mountNode.
